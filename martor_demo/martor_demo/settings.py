383c5aae8e0fa173993c4a0de8c0a34cfff33c01
Tainara Gabriel <tainara@localhost.localdomain>
2023-10-26 15:18:51 +0200

    Remove unused modules and corrects styles in setting.py file

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
-import tempfile
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,5 +141,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = BASE_DIR /  'martor_static'
+STATIC_ROOT = BASE_DIR / 'martor_static'
 MEDIA_ROOT = BASE_DIR / 'martor_media'
